2fa391616b41b1aa82a50fa8da81a52863876510
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-09-15 13:25:16 -0700

    added forms and typography plugins to the tailwindcss confic file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+  ],
   daisyui: {
     styled: true,
     themes: ["retro", "luxury"],
